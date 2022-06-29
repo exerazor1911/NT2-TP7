f8f4a58b0fbe3aef577e845e125c735e62f08252
exerazor1911 <ezequielgiussani56@gmail.com>
2022-06-28 22:37:46 -0300

    Finished

---
+++ README.md
@@ -0,0 +1,27 @@
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+#� �P�A�R�C�I�A�L�-�N�T�2�
+�
+�"# nt2-tp7"  git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/DylanKuitca/nt2-tp7.git git push -u origin main
---
+++ src/components/Tabla.vue
@@ -0,0 +1,72 @@
+<template>
+
+  <section class="src-components-tabla">
+    <div class="container-fluid mt-3">
+      <div class="jumbotron">
+
+        <div v-if="personas.length" class="table-responsive"> 
+          <table class="table table-dark align-middle align-self-center">
+              <tr> 
+                  <th>Indice</th>
+                  <th>Nombre</th>
+                  <th>Edad</th>
+                  <th>Mail</th>
+              </tr>
+              <tr v-for="(personas, index) in personas" :key="index"> 
+                  <td>{{index + 1}}</td>
+                  <td>{{personas.Nombre}}</td>
+                  <td>{{personas.Edad}}</td>
+                  <td>{{personas.Mail}}</td>
+              </tr>
+          </table>
+        </div>
+        <h4 v-else class="alert alert-danger text-center">No se encontraron personas en MockApi</h4>
+
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+
+  export default  {
+    name: 'src-components-tabla',
+    props: [],
+    mounted () {
+      this.getPostMockApi()
+    },
+    data () {
+      return {
+         url: 'https://62856fea96bccbf32d62d26c.mockapi.io/TP7',
+        personas: []
+      }
+    },
+    methods: {
+    async getPostMockApi() {
+        try {
+          let { data } = await axios(this.url)
+          this.personas = data
+        } catch (error) {
+          console.error('Error Axios', error);
+        }
+      }
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-tabla {
+  }
+  .jumbotron {
+    background-color: rgb(64, 210, 255);
+  }
+
+</style>
